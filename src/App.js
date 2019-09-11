99caa3013352ed5540aeb1f319d03e0153549d3e
bea03 <nicole.beamer@icloud.com>
2019-09-11 17:02:51 -0600

    starting events

--- src/App.js
+++ src/App.js
@@ -57,3 +57,13 @@ const mapStateToProps = (state) => {
 
 export default connect(mapStateToProps, {})(App);
 
+// STEP 3 - connecting components
+// Call the connect function twice (curry) and pass in the component
+// to the second call
+
+// The first call takes in two arguments:
+// 1- function (usually called mapStateToProps (mSTP))
+// 2- an object that takes in action creators, and wraps them in the dispatch function and passes them on to props
+
+
+
